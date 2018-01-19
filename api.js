561b2ac3145c63a11a63d4f95edd80c5b5470123
Éderson Fernandes <edersonluis.rf@gmail.com>
2018-01-19 15:23:32 -0200

    ajustando a api

--- api.js
+++ api.js
@@ -1,9 +1,23 @@
-//MERCADO BITCOIN
+// MERCADO BITCOIN
 const unirest = require('unirest')
+const crypto  = require('crypto')
+const qs      = require('querystring')
+
 const ENDPOINT_API = 'https://www.mercadobitcoin.com.br/api/'
+const ENDPOINT_TRADE_PATH = "/tapi/v3/"
+const ENDPOINT_TRADE_API = 'https://www.mercadobitcoin.net' + ENDPOINT_TRADE_PATH
 
 var MercadoBitcoin = function (config) {
     this.config = {
+        CURRENCY: config.currency
+    }
+}
+
+var MercadoBitcoinTrade = function (config) {
+    this.config = {
+        KEY: config.key,
+        SECRET: config.secret,
+        PIN: config.pin,
         CURRENCY: config.currency
     }
 }
@@ -35,4 +49,57 @@ MercadoBitcoin.prototype = {
     }
 }
 
-module.exports = {MercadoBitcoin}
+MercadoBitcoinTrade.prototype = {
+
+    getAccountInfo: function(success, error) {
+        this.call('get_account_info', {}, success, error)
+    },
+
+    listMyOrders: function (parameters, success, error) {
+        this.call('list_orders', parameters, success, error)
+    },
+
+    placeBuyOrder: function(qty, limit_price, success, error){
+        this.call('place_buy_order', {coin_pair: `BRL${this.config.CURRENCY}`, quantity: (''+qty).substr(0,10), limit_price: ''+limit_price}, success, error)
+    },
+
+    placeSellOrder: function(qty, limit_price, success, error){
+        this.call('place_sell_order', {coin_pair: `BRL${this.config.CURRENCY}`, quantity: (''+qty).substr(0,10), limit_price: ''+limit_price}, success, error)
+    },
+
+    cancelOrder: function (orderId, success, error) {
+        this.call('cancel_order', {coin_pair: `BRL${this.config.CURRENCY}`, order_id: orderId}, success, error)
+    },
+
+    call: function (method, parameters, success, error) {
+
+        var now = Math.round(new Date().getTime() / 1000)
+        var queryString = qs.stringify({'tapi_method': method, 'tapi_nonce': now})
+        if(parameters) queryString += '&' + qs.stringify(parameters)
+
+        var signature = crypto.createHmac('sha512', this.config.SECRET)
+                             .update(ENDPOINT_TRADE_PATH + '?' + queryString)
+                             .digest('hex')
+
+        unirest.post(ENDPOINT_TRADE_API)
+              .headers({'TAPI-ID': this.config.KEY})
+              .headers({'TAPI-MAC': signature})
+              .send(queryString)
+              .end(function (response) {
+                  if(response.body){
+                     if (response.body.status_code === 100 && success)
+                         success(response.body.response_data)
+                     else if(error)
+                         error(response.body.error_message)
+                     else
+                         console.log(response.body)
+                  }
+                  else console.log(response)
+              })
+    }
+}
+
+module.exports = {
+    MercadoBitcoin,
+    MercadoBitcoinTrade
+}
